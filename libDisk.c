3a96e2fdd98b70225203abb15884bca1c8276e8b
Zhi He <zhe10@unix2.csc.calpoly.edu>
2024-03-01 20:29:20 -0800

    placed include in libDisk.h

--- libDisk.c
+++ libDisk.c
@@ -1,13 +1,5 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 
-#define BLOCKSIZE 256 // size of disk block in bytes
-#define OFLAGS O_WRONLY | O_CREAT | O_TRUNC
-#define RWPERMS S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH
 
-int total_disk_space;
-char default_file = "default";
 
 int openDisk(char *filename, int nBytes) {
   /* This functions opens a regular UNIX file and designates the first
--- libDisk.h
+++ libDisk.h
@@ -1,5 +1,15 @@
 #define BLOCKSIZE 256 // size of disk block in bytes
 
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define OFLAGS O_WRONLY | O_CREAT | O_TRUNC
+#define RWPERMS S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH
+
+int total_disk_space;
+char default_file = "default";
+
 int openDisk(char *filename, int nBytes);
 int closeDisk(int disk);
 int readBlock(int disk, int bNum, void *block);
