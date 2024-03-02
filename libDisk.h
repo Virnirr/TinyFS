395c3f786757446bead22b93224ef465f853a09d
Zhi He <zhe10@unix2.csc.calpoly.edu>
2024-03-01 20:26:02 -0800

    added liDisk.h contents

--- libDisk.h
+++ libDisk.h
@@ -1,2 +1,6 @@
 #define BLOCKSIZE 256 // size of disk block in bytes
 
+int openDisk(char *filename, int nBytes);
+int closeDisk(int disk);
+int readBlock(int disk, int bNum, void *block);
+void writeBlock(int disk, int bNum, void *block);
