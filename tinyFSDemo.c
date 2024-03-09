cecca053db4fd33e4485718896b95dc772b47def
Zhi He <zhe10@unix3.csc.calpoly.edu>
2024-03-09 10:00:36 -0800

    added

---
+++ tinyFSDemo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "tinyFS.h"
+#include <assert.h> // unit test
+/* usage void assert(scalar expression); */
+#include <string.h>
+#include "TinyFS_errno.h"
+
+void test_openDisk(char*filename, int nBytes);
+
+int main() {
+
+  printf("Hello World!\n");
+
+  test_openDisk("this_is_too_long_of_name", BLOCKSIZE * 3);
+
+  return 0;
+}
+
+void test_openDisk(char*filename, int nBytes) {
+  // too small error
+  if ((int) (nBytes / BLOCKSIZE) < 2) {
+    assert(tfs_mkfs(filename, nBytes) == EMINLIMIT);
+  }
+
+  // name too long error
+  else if (strlen(filename) > FILENAME_SIZE) {
+    assert(tfs_mkfs(filename, nBytes) == OPENDISKERROR);
+  }
+
+  // make sure that otherwise, it's a valid file descriptor that you opened
+  else {
+    assert(tfs_mkfs(filename, nBytes) > 0);
+  }
+}
